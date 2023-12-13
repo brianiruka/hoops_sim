1a5d6e128e7d6bdf442aff0b58140fe6a04b9b8f
Brian Iruka <biruka3@gatech.edu>
2023-12-13 18:25:00 -0500

    randomness along x and y during motion to mimic human walking motion

--- movement.js
+++ movement.js
@@ -29,20 +29,24 @@ function updatePlayers() {
 function movePlayers() {
     for (const player of players) {
         if (player.isMoving) {
+
             // Move towards the destination
-            const speed = 2;
-            const dx = player.destinationX - player.x;
+            const speed = getRandomBetween(1, 3);
+            const dx = player.destinationX - player.x
             const dy = player.destinationY - player.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance > speed) {
-                player.x += (dx / distance) * speed;
-                player.y += (dy / distance) * speed;
+                player.x += (dx / distance) * speed + getRandomBetween(-.5, .5);;
+                player.y += (dy / distance) * speed + getRandomBetween(-.5, .5);
             } else {
-                // Player reached the destination, generate a new destination
-                player.x = player.destinationX;
-                player.y = player.destinationY;
+                // Player reached the destination, stop moving
+                player.isMoving = false;
             }
         }
     }
+}
+
+function getRandomBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
