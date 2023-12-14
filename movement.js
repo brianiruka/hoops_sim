2368bc7ef25de5a51e1f4e5148c73c01a2782890
Brian Iruka <biruka3@gatech.edu>
2023-12-13 19:54:37 -0500

    add player constructor, team constructor, positioning

--- movement.js
+++ movement.js
@@ -5,33 +5,13 @@ function movePlayerTo(player, canvas) {
     player.destinationY = Math.random() * canvas.height;
 }
 
-// Function to update players' positions
-function updatePlayers() {
-    players.forEach((player) => {
-        if (player.isMoving) {
-            const speed = 2;
-            const dx = player.destinationX - player.x;
-            const dy = player.destinationY - player.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance > speed) {
-                player.x += (dx / distance) * speed;
-                player.y += (dy / distance) * speed;
-            } else {
-                // Player reached the destination, stop moving
-                player.isMoving = false;
-            }
-        }
-    });
-}
-
 // Function to move players randomly
 function movePlayers() {
     for (const player of players) {
         if (player.isMoving) {
 
             // Move towards the destination
-            const speed = getRandomBetween(1, 3);
+            const speed = player.getMovementSpeed();
             const dx = player.destinationX - player.x
             const dy = player.destinationY - player.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
--- players.js
+++ players.js
@@ -1,91 +1,81 @@
-// Initial set of players
-players = [{
-        x: 50,
-        y: 50,
-        color: "#ffcc00",
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 200,
-        y: 100,
-        color: "#ffcc00",
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 150,
-        y: 300,
-        color: "#ffcc00",
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 400,
-        y: 250,
-        color: "#ffcc00",
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 500,
-        y: 150,
-        color: "#ffcc00",
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 850,
-        y: 50,
-        color: '#800080',
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 700,
-        y: 100,
-        color: '#800080',
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 750,
-        y: 300,
-        color: '#800080',
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 500,
-        y: 250,
-        color: '#800080',
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    },
-    {
-        x: 400,
-        y: 150,
-        color: '#800080',
-        isMoving: false,
-        destinationX: 0,
-        destinationY: 0
-    }
-];
+// Base Player constructor
+function Player(x, y) {
+    this.x = x;
+    this.y = y;
+    this.isMoving = false;
+    this.destinationX = x;
+    this.destinationY = y;
+    this.team = null; // Initially no team
+}
+// Team constructor
+function Team(name, color) {
+    this.name = name;
+    this.color = color;
+    this.players = []; // Array to store players
+    this.isOnOffense = false;
+}
 
+// Method to switch the team's state between offense and defense
+Team.prototype.switchState = function () {
+    this.isOnOffense = !this.isOnOffense;
+};
+
+// Function to create a player and add it to a team
+function createPlayer(team, x, y, position) {
+    const player = new Player(x, y, team.color);
+    player.team = team;
+    player.color = team.color;
+    player.position = position;
+    team.players.push(player);
+    players.push(player); // Add player to the players array
+    return player;
+}
+const players = []; // Array to store players
+// Create two teams
+const teamA = new Team('Team A', '#ffcc00');
+const teamB = new Team('Team B', '#800080');
+
+// Create players and assign them to teams
+const player1 = createPlayer(teamA, 50, 15, "pg");
+const player2 = createPlayer(teamA, 90, 15, "sg");
+const player3 = createPlayer(teamA, 130, 15, "sf");
+const player4 = createPlayer(teamA, 170, 15, "pf");
+const player5 = createPlayer(teamA, 210, 15, "c");
+
+const player6 = createPlayer(teamB, 850, 15, "pg");
+const player7 = createPlayer(teamB, 810, 15, "sg");
+const player8 = createPlayer(teamB, 770, 15, "sf");
+const player9 = createPlayer(teamB, 730, 15, "pf");
+const player10 = createPlayer(teamB, 690, 15, "c");
 // Function to draw a player
-function drawPlayer(x, y, color) {
+function drawPlayer(player) {
     ctx.beginPath();
-    ctx.arc(x, y, 15, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
+    ctx.arc(player.x, player.y, 15, 0, 2 * Math.PI);
+    ctx.fillStyle = player.color;
     ctx.fill();
     ctx.closePath();
+
+    ctx.fillStyle = '#ffffff';
+    ctx.font = '12px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(player.position.toUpperCase(), player.x, player.y);
 }
+
+// Method to get movement speed based on player's position
+Player.prototype.getMovementSpeed = function () {
+    switch (this.position) {
+        case 'pg':
+            return 3; // Faster speed for point guards
+        case 'sg':
+            return 2.5;
+        case 'sf':
+            return 2;
+        case 'pf':
+            return 1.5;
+        case 'c':
+            return 1; // Slower speed for centers
+        default:
+            return 2; // Default speed if position is not specified
+    }
+};
